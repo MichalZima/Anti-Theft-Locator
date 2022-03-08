0155bd8321ff21251ca39c346a2dd701d3a90d71
MichalZima <michal.zima01@gmail.com>
2022-03-08 14:25:46 +0100

    removed serial prints

--- main/LED.h
+++ main/LED.h
@@ -22,20 +22,13 @@ class LED {
     /*----------------------------------------------------------------------------------------------------------------------------------------*/
 
     void Test() {
-      Serial.println("\n1. segment start");
       digitalWrite(greenLedPin, HIGH);
-      Serial.println("  green on");
       delay(1000);      
       digitalWrite(greenLedPin, LOW);
-      Serial.println("  green off");      
-      Serial.println("  red on");
       digitalWrite(redLedPin, HIGH);
       delay(1000);
-      digitalWrite(redLedPin, LOW); 
-      Serial.println("  red off");     
+      digitalWrite(redLedPin, LOW);     
       delay(1000);
-
-      Serial.println("1. segment done");
 
       Blik(250, "red", 3);
       delay(1000);
@@ -44,16 +37,12 @@ class LED {
       Blik(250, "green", 3);
       delay(1000);   
 
-      Serial.println("2. segment done");
-
       Light("green", 1000);
       delay(1000);
       Light("orange", 1000);
       delay(1000);
       Light("red", 1000);
       delay(1000);
-
-      Serial.println("3. segment done");
     }
 
     /*----------------------------------------------------------------------------------------------------------------------------------------*/     
@@ -61,11 +50,8 @@ class LED {
     void Blik(const long INTERVAL, char COLOR[], int LOOPS) {
       uint32_t previousMillis = millis();
       uint8_t i = 0;
-      Serial.println("print 2.1");
 
       while (i < LOOPS*2) {
-        Serial.println("print 2.2");    
-        Serial.println("print 2.3");  
 
         if (COLOR == "green") {
           digitalWrite(greenLedPin, ledState);
@@ -81,7 +67,6 @@ class LED {
         }
                         
         if (millis() - previousMillis >= INTERVAL/2) {
-          Serial.println("print 2.4");
           ledState = !ledState;
           previousMillis = millis();
           i++;          
@@ -114,7 +99,6 @@ class LED {
 
     void Light(char COLOR[8], uint32_t INTERVAL) {
       uint32_t previousMillis = millis();
-      Serial.println("print 3.1");
            
       if (COLOR == "green") {
         digitalWrite(greenLedPin, HIGH);
